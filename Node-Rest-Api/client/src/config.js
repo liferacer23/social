112eacd5768502ba72351d286b08e3072a40b838
liferacer23 <binyamg23@gmail.com>
2022-04-27 02:53:55 +0300

    deloyment

--- Node-Rest-Api/client/src/config.js
+++ Node-Rest-Api/client/src/config.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
 
-    baseURL: "https://hollasocial.heroku.com/api"
+    baseURL: "https://hollasocial.herokuapp.com/api"
 })
